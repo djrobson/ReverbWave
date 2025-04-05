88cd66f80916911034e96f8d802731cc6689490d
djrobson <djrobson@users.noreply.github.com>
2025-04-05 10:31:29 -0400

    moved all source into Source/, cleaned up JuceLibrary build artifact

--- harmonic_variables.cpp
+++
@@ -1,6 +0,0 @@
-// Variables for harmonic detuning
-std::vector<float> oddHarmonicBufferL;
-std::vector<float> evenHarmonicBufferR;
-int oddHarmonicPos = 0;
-int evenHarmonicPos = 0;
-static const int harmonicBufferSize = 50;
